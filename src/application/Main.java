e18400fd359c281dbc954e98a827b92d46dba287
florian berden <florian.berden@student.ucll.be>
2018-12-24 11:42:03 +0100

    Main + DatabaseFacade kuis

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,29 +2,14 @@ package application;
 
 import controller.Quizcontroller;
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import view.panels.AssesMainPane;
-import view.panels.CategoryDetailPane;
-import view.panels.CategoryOverviewPane;
-import view.panels.MessagePane;
-import view.panels.QuestionDetailPane;
-import view.panels.QuestionOverviewPane;
-import view.panels.TestPane;
 
 public class Main extends Application {
-	private Quizcontroller quizcontroller;
 
 	@Override
 	public void start(Stage primaryStage) {
-
-		quizcontroller = Quizcontroller.getInstance();
+		Quizcontroller quizcontroller = Quizcontroller.getInstance();
 		quizcontroller.start(quizcontroller);
-
-
-
 	}
 
 	public static void main(String[] args) {
--- src/database/DatabaseFacade.java
+++ src/database/DatabaseFacade.java
@@ -20,48 +20,13 @@ public class DatabaseFacade {
     public ArrayList<ArrayList<String>> loadTest(){
         context.setDBStrategy(currentsource);
         return context.load();
-/*
-        NOG BETER
-        context.setDBStrategy();
-        return context.load();
-*/
     }
 
     public void saveTest(ArrayList elementen){
         context.setDBStrategy(currentsource);
         context.save(elementen);
-/*
-        NOG BETER
-        context.setDBStrategy();
-        return context.save(elementen);
-*/
     }
 
-    //einde voorbeeld
-
-    /*
-    OOK DEZE MOETEN VERVANGEN WORDEN -TB
-    public void saveCategories(ArrayList<String> categoryElements) {
-        context.setDBStrategy(new CategoryText());
-        context.save(categoryElements);
-    }
-
-    public void saveQuestions(ArrayList<String> questionElements) {
-        context.setDBStrategy(new QuestionText());
-        context.save(questionElements);
-    }
-
-
-    public ArrayList<String> loadCategorieElementen() {
-        context.setDBStrategy(new CategoryText());
-        return context.load();
-    }
-
-    public ArrayList<String> loadQuestionElementen(){
-        context.setDBStrategy(new QuestionText());
-        return context.load();
-    }
-     */
 
     public ArrayList<String> loadEvalutionStrategy() {
         context.setDBStrategy(new PropertyStrategy());
